58b93fa623f6dd5c9e7a96ce88d1c2118ab0336f
Otsile <tsheleotsile@gmail.com>
2023-07-13 13:46:35 +0200

    Added all Java Files with source code

---
+++ Java-Calculator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package loginpage;
+
+/**
+ *
+ * @author tshel
+ */
+public class LoginPage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      frmMain frmmain = new frmMain();
+      frmmain.show();
+     
+    }
+}
+
+  
